6739145438df943c3f2de249242af844ecdc6521
jeremie.daviaud <jeremie.daviaud@my-orleans.fr>
2018-01-08 15:06:09 +0100

    creation location

---
+++ src/MyOrleansBundle/Controller/front/LocationController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabiendurand
+ * Date: 08/01/2018
+ * Time: 14:46
+ */
+
+namespace MyOrleansBundle\Controller\front;
+
+
+use MyOrleansBundle\Entity\Client;
+use MyOrleansBundle\Entity\Location;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class LocationController extends Controller
+{
+    public function locationAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $locations = $em->getRepository(Location::class)->findAll();
+
+        // Formulaire de contact
+        $telephoneNumber = $this->getParameter('telephone_number');
+
+        $client = new Client();
+        $formulaire = $this->createForm('MyOrleansBundle\Form\FormulaireType', $client);
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $mailer = $this->get('mailer');
+
+            $message = new \Swift_Message('Nouveau message de my-orleans.com');
+            $message
+                ->setFrom($client->getEmail())
+                ->setTo($this->getParameter('mailer_user'))
+
+
+                ->setBody(
+                    $this->renderView(
+
+                        'MyOrleansBundle::receptionForm.html.twig',
+                        array('client' => $client)
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            $client->setDate(new \Datetime());
+
+            $em->persist($client);
+            $em->flush();
+
+            $this->addFlash('success', 'votre message a bien été envoyé');
+
+            return $this->redirectToRoute('immo_pratique');
+        }
+
+        return $this->render('MyOrleansBundle::locations.html.twig', [
+            'locations' => $locations,
+            'telephone_number' =>$telephoneNumber,
+            'form' => $formulaire->createView(),
+        ]);
+    }
+}
