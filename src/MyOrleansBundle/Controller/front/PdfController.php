8b31a41ad87684515f39544ae11cfc297007d0ef
SubN3o <jeremie.daviaud@sfr.fr>
2018-01-11 14:19:37 +0100

    controller pdf location OK

--- src/MyOrleansBundle/Controller/front/PdfController.php
+++ src/MyOrleansBundle/Controller/front/PdfController.php
@@ -10,11 +10,13 @@ namespace MyOrleansBundle\Controller\front;
 
 
 use MyOrleansBundle\Entity\Flat;
+use MyOrleansBundle\Entity\Location;
 use MyOrleansBundle\Entity\Media;
 use MyOrleansBundle\Entity\Residence;
 use MyOrleansBundle\Entity\TypeBien;
 use MyOrleansBundle\Entity\TypeLogement;
 use MyOrleansBundle\Service\CalculateurCaracteristiquesResidence;
+use MyOrleansBundle\Service\CalculateurHonoraires;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -99,6 +101,49 @@ class PdfController extends Controller
         ));
 
         $filename = $flat->getTypeBien()->getNom()."-".$flat->getReference().".pdf";
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+            ]);
+
+    }
+
+    /**
+     * Retrun a pdf file from a location.
+     * @return Response
+     * @Route("/pdf/location/{reference}.pdf", name="location_pdf")
+     * @ParamConverter("location", class="MyOrleansBundle:Location", options={"reference" = "reference"})
+     * @Method("GET")
+     */
+    public function pdfLocationAction(Location $location, Request $request, CalculateurHonoraires $calculateurHonoraires)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        //calcul des honoraires selon la surface
+        $honoraires = $calculateurHonoraires->calculHonoraires($location->getSurface());
+
+        $mailagence = $this->getParameter('mail_agence');
+        $telephoneNumber = $this->getParameter('telephone_number');
+        $snappy = $this->get("knp_snappy.pdf");
+        $medias = $location->getMedias();
+
+
+        $html = $this->renderView('MyOrleansBundle::pdf_location.html.twig', array(
+            'base_dir' => $this->get('kernel')->getRootDir() . '/../web' . $request->getBasePath(),
+            'location'=>$location,
+            'honoraires'=>$honoraires,
+            'medias' => $medias,
+            'telephone_number' => $telephoneNumber,
+            'mail_agence'=>$mailagence,
+
+        ));
+
+        $filename = $location->getTypeBien()->getNom()."-".$location->getReference().".pdf";
 
         return new Response(
             $snappy->getOutputFromHtml($html),
